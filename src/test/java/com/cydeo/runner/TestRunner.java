8b693811bbc48782b8d2bb3d4e8f98c4538ad031
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-11-11 13:12:09 -0600

    Failed TestRunner class added

--- src/test/java/com/cydeo/runner/TestRunner.java
+++ src/test/java/com/cydeo/runner/TestRunner.java
@@ -1,4 +1,22 @@
 package com.cydeo.runner;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions( features="src/test/resources/features" ,
+        glue="com/library/step_definitions",
+        publish = true,
+        plugin = {"html:target/cucumber.html" ,
+                "rerun:target/rerun.txt" ,// store the failed scenario into rerun.txt
+
+        },
+
+        dryRun = false
+      //  ,tags="@us06"
+
+)
 public class TestRunner {
 }
